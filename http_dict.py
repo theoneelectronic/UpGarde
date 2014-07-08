d4986ed7ca11b987f99120a43fb4280fed49df8d
Gabriele Visconti <gabriele.visconti@gmail.com>
2014-07-08 22:46:48 +0200

    HTTP dictionary
    This is a custom Python module that simply contains a dictionary of
    HTTP status codes

---
+++ http_dict.py
@@ -0,0 +1,11 @@
+
+http_status_dict = {"100": "100 Continue : Server has received the request headers and client should proceed to send the request body",
+                    "101": "101 Switching Protocols : The requester has asked the server to switch protocols and the server is acknowledging that it will do so",
+                    "102": "102 Processing : Server has received and is processing the request, but no response is available yet",
+                    "200": "200 OK : Standard response for successful HTTP requests.",
+                    "201": "201 Created : The request has been fulfilled and resulted in a new resource being created.",
+                    "202": "202 Accepted : The request has been accepted for processing, but the processing has not been completed.",
+                    "203": "203 Non-Authoritative Information : The server successfully processed the request, but is returning information that may be from another source.",
+                    "204": "204 No Content : The server successfully processed the request, but is not returning any content. Usually used as a response to a successful delete request.",
+                    
+                    }
