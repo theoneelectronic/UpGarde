2e52f348ce6ff724d105374657dbf1d11338a4a8
Gabriele Visconti <gabriele.visconti@gmail.com>
2014-07-08 22:44:17 +0200

    Main
    this is the main Python program. It contains the main functions and
    call the proper modules

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import threading
+from http_dict import http_status_dict
+import pymongo
+from pymongo import MongoClient
+
+global req
+req = requests.get('https://www.python.org/')
+
+def get_http_status():
+    #threading.Timer(20, get_http_status).start()
+    global req_stat
+    req_stat = req.status_code
+          
+def get_host_headers():
+    #threading.Timer(30, get_host_headers).start()
+    global headers
+    headers = req.headers
+    print json.dumps(dict(headers))
+    print "\n"
+
+get_http_status()
+get_host_headers()
+
+print http_status_dict[str(req_stat)]
+
+"""
+client1 = MongoClient()
+db = client1.test_database1
+
+post = dict(headers)
+
+posts = db.posts
+post_id = posts.insert(post)
+"""
+
+
+
