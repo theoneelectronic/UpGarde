1cb5e510a21bd8accc3f9b923273733d806feab0
Gabriele Visconti <gabriele.visconti@gmail.com>
2014-07-09 15:31:43 +0200

    updated the http_dict with 2xx Success codes

--- http_dict.py
+++ http_dict.py
@@ -7,5 +7,11 @@ http_status_dict = {"100": "100 Continue : Server has received the request heade
                     "202": "202 Accepted : The request has been accepted for processing, but the processing has not been completed.",
                     "203": "203 Non-Authoritative Information : The server successfully processed the request, but is returning information that may be from another source.",
                     "204": "204 No Content : The server successfully processed the request, but is not returning any content. Usually used as a response to a successful delete request.",
+                    "205": "205 Reset Content : The server successfully processed the request, but is not returning any content.",
+                    "206": "206 Partial Content : The server is delivering only part of the resource due to a range header sent by the client.",
+                    "207": "207 Multi-Status (WebDAV; RFC 4918) : The message body that follows is an XML message and can contain a number of separate response codes, depending on how many sub-requests were made.",
+                    "208": "208 Already Reported (WebDAV; RFC 5842) : The members of a DAV binding have already been enumerated in a previous reply to this request, and are not being included again.",
+                    "226": "226 IM Used (RFC 3229) : The server has fulfilled a GET request for the resource, and the response is a representation of the result of one or more instance-manipulations applied to the current instance.",
+                    
                     
                     }
