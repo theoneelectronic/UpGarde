696e81b194dbaf7849990e07fa687bc188d10c30
Gabriele Visconti <gabriele.visconti@gmail.com>
2014-08-28 17:01:43 +0200

    module containing a dictionary of user agents and a list of user agent names to use as list box labels and as keys for the user agent dictionary within the program

---
+++ GUI_mode/ua_dict.py
@@ -0,0 +1,62 @@
+ua_dict = {'Chrome 36.0 Win7 64-bit': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+           'Chrome 36.0 MacOSX': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+           'Firefox 31.0 Win7 64-bit': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0',
+           'Safari 7.0 MacOSX': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.77.4 (KHTML, like Gecko) Version/7.0.5 Safari/537.77.4',
+           'Chrome 36.0 Win7 64-bit': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
+           'Chrome 36.0 MacOSX': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
+           'Firefox 31.0 MacOSX': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0',
+           'Chrome 36.0 Win8.1 64-bit': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+           'Safari 7.0 MacOSX': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.78.2 (KHTML, like Gecko) Version/7.0.6 Safari/537.78.2',
+           'Chrome 36.0 Win8.1 64-bit': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
+           'Mobile Safari 7.0 iOS': 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53',
+           'IE 11.0 Win7 64-bit': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+           'Firefox 31.0 Win8.1 64-bit': 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0',
+           'Chrome 36.0 Win7 32-bit': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+           'Firefox 31.0 Linux': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
+           'Firefox 31.0 Win7 32-bit': 'Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0',
+           'Chrome 36.0 Linux': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+           'Mobile Safari 7.0 iOS': 'Mozilla/5.0 (iPad; CPU OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53',
+           'Chrome 36.0 Win7 32-bit': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
+           'Firefox 30.0 Win7 64-bit': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0',
+           'Firefox 31.0 WinXP 32-bit': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0',
+           'Chrome 36.0 Win8 64-bit': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+           'Chrome 36.0 MacOSX': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+           'Chrome 36.0 Linux': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
+           'Chrome 36.0 MacOSX': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+
+           }
+
+ua_list = ['Chrome 36.0 Win7 64-bit',
+           'Chrome 36.0 MacOSX',
+           'Firefox 31.0 Win7 64-bit',
+           'Safari 7.0 MacOSX',
+           'Chrome 36.0 Win7 64-bit',
+           'Chrome 36.0 MacOSX',
+           'Firefox 31.0 MacOSX',
+           'Chrome 36.0 Win8.1 64-bit',
+           'Safari 7.0 MacOSX',
+           'Chrome 36.0 Win8.1 64-bit',
+           'Mobile Safari 7.0 iOS',
+           'IE 11.0 Win7 64-bit',
+           'Firefox 31.0 Win8.1 64-bit',
+           'Chrome 36.0 Win7 32-bit',
+           'Firefox 31.0 Linux',
+           'Firefox 31.0 Win7 32-bit'
+           'Chrome 36.0 Linux',
+           'Mobile Safari 7.0 iOS',
+           'Chrome 36.0 Win7 32-bit',
+           'Firefox 30.0 Win7 64-bit',
+           'Firefox 31.0 WinXP 32-bit',
+           'Chrome 36.0 Win8 64-bit',
+           'Chrome 36.0 MacOSX',
+           'Chrome 36.0 Linux',
+           'Chrome 36.0 MacOSX',
+           ]
+           
+           
+           
+           
+           
+           
+           
+           
